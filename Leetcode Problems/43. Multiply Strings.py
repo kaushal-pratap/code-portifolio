1006b5cd261a388a28c8e79699b39849e10482d8
kaushal-pratap <kaushalpratap3d@gmail.com>
2024-06-22 02:15:18 +0530

    Update

--- Leetcode Problems/43. Multiply Strings.py
+++ Leetcode Problems/43. Multiply Strings.py
@@ -13,22 +13,15 @@ Output: "6"
 
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
+         
         HashMap = {
-        "1":1,
-        "2":2,
-        "3":3,
-        "4":4,
-        "5":5,
-        "6":6,
-        "7":7,
-        "8":8,
-        "9":9,
-        "0":0,
+        "1":1,"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"0":0,
         }
         int1 = 0
         int2 = 0
         i = 0
         j = 0
+        
         while i != len(num1)-1:
              int1 += HashMap[num1[i]]*(10**(len(num1)-1-i))
              i += 1
