746b97ac233946a465f021c7408deeddb6c0d5de
kaushal-pratap <kaushalpratap3d@gmail.com>
2025-05-22 02:13:09 +0530

    Latest problem optimized

--- Leetcode Problems/3355. Zero Array Transformation I.py
+++ Leetcode Problems/3355. Zero Array Transformation I.py
@@ -5,9 +5,9 @@ https://leetcode.com/problems/zero-array-transformation-i/description/
 class Solution:
     def isZeroArray(self, nums: list[int], queries: list[list[int]]) -> bool:
         diff = [0]*(len(nums)+1)
-        for i in queries:
-            diff[i[0]] += 1
-            diff[i[1]+1] -= 1
+        for left,right in queries:
+            diff[left] += 1
+            diff[right+1] -= 1
         for i in range(1,len(diff)):
             diff[i] += diff[i-1]
         for i in range(len(nums)):
